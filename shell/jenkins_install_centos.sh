264ed91de7b071da0dcc74e67b455d16f8ef62bb
Goular <zhaojt_cs@126.com>
2017-12-27 11:19:40 +0800

    添加Jenkins安装的内容

---
+++ shell/jenkins_install_centos.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins -y
+service jenkins restart
