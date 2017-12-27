ff84f880e3633115ab3547c23caf24a95fc77e0e
Goular <zhaojt_cs@126.com>
2017-12-27 12:17:23 +0800

    添加Jenkins安装的内容

--- shell/jenkins_install_centos.sh
+++ shell/jenkins_install_centos.sh
@@ -3,3 +3,5 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
 service jenkins restart
+
+# 执行完成需要将jenkins添加到root组，不然docker不能使用
